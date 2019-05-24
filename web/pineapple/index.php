a0fe9bf902215f28cc67f9317ace4f598f954ce3
Justintime50 <justinpaulhammond@gmail.com>
2019-05-23 23:25:45 -0600

    Pineapple fixes

--- web/pineapple/index.php
+++ web/pineapple/index.php
@@ -26,7 +26,6 @@
 
 <!-- Nav -->
 <nav class="navbar navbar-expand-lg fixed-top">
-    <!--<div class="container">-->
     <a class="navbar-brand" href="index.php"><h1 class="navbar-brand">PINEAPPLE</h1><img src="assets/pics/pineapple-icon.png" class="logo" alt="Pineapple"></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#myNavbar" aria-controls="myNavbar" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -34,14 +33,12 @@
 
     <div class="collapse navbar-collapse" id="myNavbar">
         <ul class="navbar-nav ml-auto">
-            <li class="nav-item"><a class="nav-link" href="docs.php">DOCUMENTATION</a></li>
+            <li class="nav-item"><a class="nav-link" href="https://github.com/Justintime50/Pineapple">DOCUMENTATION</a></li>
             <li class="nav-item"><a class="nav-link" href="#examples">EXAMPLES</a></li>
             <li class="nav-item"><a class="nav-link" href="#get-started">GET STARTED</a></li>
-            <li class="nav-item"><a class="nav-link" href="#themes">THEMES</a></li>
             <li class="nav-item"><a class="nav-link" href="#about">ABOUT</a></li>
         </ul>
     </div>
-    <!--</div>-->
 </nav>
 
 
@@ -90,7 +87,7 @@
     <br><hr><br>
 <p>Content can easily be created and customized using Pineapple's simple CSS and JS functions. Plug and play with your favorite from the collection. See our documentation for a full list of features.</p>
 <br>
-<a href="docs.php"><button class="btn btn-lg btn-other" style="margin-bottom:0px;">Documentation</button></a>
+<a href="https://github.com/Justintime50/Pineapple"><button class="btn btn-lg btn-other" style="margin-bottom:0px;">Documentation</button></a>
 
 </div>
 
@@ -98,83 +95,21 @@
 
 <!-- Container (GET STARTED Section) -->
 <div id="get-started"></div>
-<div class="container-fluid pa-bg-gray">
+    <div class="container-fluid pa-bg-gray">
 
-  <h2>Get Started</h2>
+      <h2>Get Started</h2>
 
-    <div class="text-center pa-slideanim">
-      <p>Pineapple is a downloadable project you can include in any site you build. Explore the installation options below and make sure to checkout our <a href="docs/index.php">documentation</a> for more information.</p>
-    </div>
-      <hr><br>
-    <div class="row">
-      <div class="col-md-12 text-center pa-slideanim" style="padding-bottom:20px;">
-        <div class="pa-card">
-          <h3>Pineapple Source Code</h3>
-          <hr>
-          <p>Download the compiled project here.</p>
-          <a href="pineapple/zip/pineapple/pineapple-1.3.zip"><button class="btn btn-lg btn-other" style="margin-bottom:15px;">Download Pineapple</button></a>
-          <p>(The complete CSS and Javascript library.)</p>
+        <div class="text-center pa-slideanim">
+            <p>View the complete Pineapple project on <a href="https://github.com/Justintime50/Pineapple">Github</a>.</p>
         </div>
-      </div>
-        <!--
-        <div class="col-md-6 text-center pa-slideanim">
-          <div class="pa-card">
-          <h3>Content Delivery Network</h3>
-          <hr>
-          <p>Copy and paste the links to your project.</p>
-          <span id="copy_clipboard" data-clipboard-text='<script src="https://pa-lib.com/pineapple/1.2.2/js/pineapple.min.js"></script>'>
-          <button class="btn btn-lg btn-other" style="margin-bottom:15px;" onclick="alert('Text copied! Please close this window.');">Copy JS Link</button>
-            </span>
-
-          <span id="copy_clipboard" data-clipboard-text='<script src="https://pa-lib.com/pineapple/1.2.2/css/pineapple.min.css"></script>'>
-          <button class="btn btn-lg btn-other" style="margin-bottom:15px;" onclick="alert('Text copied! Please close this window.');">Copy CSS Link</button>
-            </span>
-          <p>(Link to our CDN and we'll host Pineapple.)</p>
-        </div>
-      </div>
     </div>
-  <div class="row">
-    <div class="col-sm-12 text-left pa-slideanim">
-      <hr>
-      <h3>Pineapple CDN Instructions</h3>
-      <p>
-        <b>JS</b> - Make sure to reference our Javascript file after Bootstrap but before your personal js files.
-        <br>
-        <b>CSS</b> - Make sure to reference our CSS file after Bootstrap but before your personal css files.
-      </p>
-
-
-    </div>
-    -->
-  </div>
-</div>
-
-
-
-<!-- Container (THEMES Section) -->
-<div id="themes"></div> 
-<div class="container-fluid text-center pa-slideanim">
-  <h2>Themes</h2>
-    <div class="row">
-      <div class="col-md-12">
-        <p>We've created some awesome themes that you can use to build your website quickly.<br>Unlike our library, these must be downloaded. No CDN is available for themes.</p>
-        <br>
-        <h3>Waterfall Theme</h3>
-        <a href="http://pa-lib.com/pineapple/zip/themes/waterfall-theme.zip"><button class="btn btn-lg btn-other" style="margin-bottom:0px;">Download</button></a>
-        <br><br>
-        <h3>Coming Soon Template</h3>
-        <a href="http://pa-lib.com/pineapple/zip/themes/coming_soon_template.zip"><button class="btn btn-lg btn-other" style="margin-bottom:0px;">Download</button></a>
-      </div>
-    </div>
-    <br><hr><br>
-<p>This site is built on our waterfall theme.</p>
 </div>
 
 
 
 <!-- Container (ABOUT Section) -->
 <div id="about"></div>
-<div class="container-fluid text-center pa-bg-gray">
+<div class="container-fluid text-center">
   <h2>About</h2>
   <br>
     <h3 class="pal-font-s">Pineapple Team</h3>
@@ -207,17 +142,6 @@
             </div>
           </div>
 
-
-<!--
-          <div class="col-sm-3">
-            <div class="pa-card">
-              <h3 class="pa-icon">LO</h3>
-              <h4>Lindsey<br>Owens</h4>
-              <a href="http://neweyesbrightskies.com">Website</a>
-            </div>
-          </div>
-
-          -->
         </div>
 </div>
 
