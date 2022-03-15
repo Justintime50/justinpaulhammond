d04f3691c4c1a333057a1246f17a2cf4b0439c11
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2022-03-15 00:34:52 -0600

    fix: overhaul frontend dependencies

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,3 +1,17 @@
-require('./bootstrap');
+try {
+    // The Bootstrap Javascript library
+    require("bootstrap");
+    require("@popperjs/core");
+} catch (e) {}
 
-window.pineapple = require('pineapple-library/pineapple/dist/js/pineapple');
+/**
+ * We'll load the axios HTTP library which allows us to easily issue requests
+ * to our Laravel back-end. This library automatically handles sending the
+ * CSRF token as a header based on the value of the "XSRF" token cookie.
+ */
+
+window.axios = require("axios");
+
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+
+window.pineapple = require("pineapple-library/pineapple/dist/js/pineapple");
