2f68f1dc28ff534dfd98ec05506f60784fe41490
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2022-05-18 00:12:12 -0600

    chore: blade formatting

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM justintime50/nginx-php:8.1-11
+
 COPY --chown=www-data:www-data ./src /var/www/html
 
 RUN composer install -q --no-ansi --no-interaction --no-scripts --no-plugins --no-progress --prefer-dist \
--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -15,7 +15,10 @@
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-140798052-1"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
         gtag('js', new Date());
         gtag('config', 'UA-140798052-1');
     </script>
