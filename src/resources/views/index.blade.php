cb21ecbb330336e1f35b3d65a41a587bce89d88e
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2023-01-11 13:35:33 -0700

    chore: php 8.2, bump deps, cleanup index

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -60,14 +60,10 @@
             since 16, and founding tech companies since 20. I'm an open source fanatic, Apple fanboy, and love to
             explore new tech. I spend my time coding open source projects, tinkering with electronics and new tech
             products, and consulting teams on how to get things done.</p>
+
         <div class="row text-center">
             <div class="col pa-slideanim">
-                <a href="https://github.com/Justintime50/Justintime50/blob/main/resume.md" target="_blank">
-                    <i class="fas fa-file-pdf fa-4x"></i>
-                </a>
-            </div>
-            <div class="col pa-slideanim">
-                <a href="https://github.com/justintime50" target="_blank">
+                <a href="https://github.com/Justintime50" target="_blank">
                     <i class="fab fa-github fa-4x"></i>
                 </a>
             </div>
@@ -76,8 +72,27 @@
                     <i class="fab fa-linkedin-in fa-4x"></i>
                 </a>
             </div>
+            <div class="col pa-slideanim">
+                <a href="https://www.youtube.com/c/JustinHammond" target="_blank">
+                    <i class="fab fa-youtube fa-4x"></i>
+                </a>
+            </div>
+            <div class="col pa-slideanim">
+                <a href="https://blog.justinpaulhammond.com" target="_blank">
+                    <i class="fas fa-blog fa-4x"></i>
+                </a>
+            </div>
+            <div class="col pa-slideanim">
+                <a href="https://trailhead.salesforce.com/en/me/justinhammond" target="_blank">
+                    <i class="fab fa-salesforce fa-4x"></i>
+                </a>
+            </div>
+            <div class="col pa-slideanim">
+                <a href="https://github.com/Justintime50/Justintime50/blob/main/resume.md" target="_blank">
+                    <i class="fas fa-file-pdf fa-4x"></i>
+                </a>
+            </div>
         </div>
-    </div>
     </div>
 
     <div class="bg-blue">
@@ -93,7 +108,7 @@
                 </div>
                 <div class="col pa-slideanim">
                     <i class="fab fa-node-js fa-3x"></i>
-                    <h4>Node/JS</h4>
+                    <h4>JS/Node</h4>
                 </div>
                 <div class="col pa-slideanim">
                     <i class="fab fa-php fa-3x"></i>
@@ -104,8 +119,8 @@
                     <h4>Ruby</h4>
                 </div>
                 <div class="col pa-slideanim">
-                    <i class="fas fa-terminal fa-3x"></i>
-                    <h4>Bash</h4>
+                    <i class="fa-brands fa-golang fa-3x"></i>
+                    <h4>Golang</h4>
                 </div>
             </div>
             <div class="row">
@@ -155,58 +170,25 @@
         </div>
     </div>
 
-    <div class="container text-center">
-        <h2>Find Me on the Web</h2>
-        <div class="row">
-            <div class="col pa-slideanim">
-                <a href="https://github.com/Justintime50" target="_blank">
-                    <i class="fab fa-github fa-5x"></i>
-                </a>
-            </div>
-            <div class="col pa-slideanim">
-                <a href="https://linkedin.com/in/justin-hammond" target="_blank">
-                    <i class="fab fa-linkedin-in fa-4x"></i>
-                </a>
-            </div>
-            <div class="col pa-slideanim">
-                <a href="https://www.youtube.com/c/JustinHammond" target="_blank">
-                    <i class="fab fa-youtube fa-5x"></i>
-                </a>
-            </div>
-            <div class="col pa-slideanim">
-                <a href="https://blog.justinpaulhammond.com" target="_blank">
-                    <i class="fas fa-blog fa-5x"></i>
-                </a>
-            </div>
-            <div class="col pa-slideanim">
-                <a href="https://trailhead.salesforce.com/en/me/justinhammond" target="_blank">
-                    <i class="fab fa-salesforce fa-5x"></i>
-                </a>
-            </div>
-        </div>
-    </div>
-
-    <div class="bg-blue">
-        <div class="container">
-            <h2>News &#38; Articles</h2>
-            {{-- TODO: Add data from an API call to Laraview so it auto-updates --}}
-            <ul class="news-table-items">
-                <li class="pa-slideanim"><a
-                        href="https://www.easypost.com/blog/2022-08-04-lessons-learned-overhauling-client-libraries-across-seven-languages"
-                        target="_blank">Lessons Learned Overhauling Client Libraries Across 7 Languages (EasyPost)</a>
-                </li>
-                <li class="pa-slideanim"><a
-                        href="https://utah.comcast.com/2013/05/20/congratulations-comcast-leaders-and-achievers-scholarship-winners/"
-                        target="_blank">Utah $10,000 Leaders &#38; Achievers Award Recipient</a></li>
-                <li class="pa-slideanim"><a href="https://chhighschool.wordpress.com/tag/justin-hammond/"
-                        target="_blank">Copper Hills High School Comcast Scholarship Recognition</a></li>
-                <li class="pa-slideanim"><a
-                        href="https://thoriumsim.com/blog/2019/04/01/lessons-learned-a-new-perspective"
-                        target="_blank">Lessons Learned Running a Space Simulator</a></li>
-                <li class="pa-slideanim">See <a href="https://blog.justinpaulhammond.com" target="_blank">my blog</a>
-                    for additional articles.</li>
-            </ul>
-        </div>
+    <div class="container">
+        <h2>News &#38; Articles</h2>
+        {{-- TODO: Add data from an API call to Laraview so it auto-updates --}}
+        <ul class="news-table-items">
+            <li class="pa-slideanim"><a
+                    href="https://www.easypost.com/blog/2022-08-04-lessons-learned-overhauling-client-libraries-across-seven-languages"
+                    target="_blank">Lessons Learned Overhauling Client Libraries Across 7 Languages (EasyPost)</a>
+            </li>
+            <li class="pa-slideanim"><a
+                    href="https://utah.comcast.com/2013/05/20/congratulations-comcast-leaders-and-achievers-scholarship-winners/"
+                    target="_blank">Utah $10,000 Leaders &#38; Achievers Award Recipient</a></li>
+            <li class="pa-slideanim"><a href="https://chhighschool.wordpress.com/tag/justin-hammond/"
+                    target="_blank">Copper Hills High School Comcast Scholarship Recognition</a></li>
+            <li class="pa-slideanim"><a
+                    href="https://thoriumsim.com/blog/2019/04/01/lessons-learned-a-new-perspective"
+                    target="_blank">Lessons Learned Running a Space Simulator</a></li>
+            <li class="pa-slideanim">See <a href="https://blog.justinpaulhammond.com" target="_blank">my blog</a>
+                for additional articles.</li>
+        </ul>
     </div>
 
     <footer class="text-center">
