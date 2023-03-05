9e8aa73cae703212fc7248d49927a77677b3c881
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2023-03-05 02:09:05 -0700

    fix: setup script

--- setup.sh
+++ setup.sh
@@ -17,6 +17,23 @@ main() {
 
     # Spin up the project (assumes you already have Traefik setup)
     docker compose up -d --build --quiet-pull
+
+    # Run a healthcheck to ensure the container is running
+    for ((ATTEMPT = 0; ATTEMPT <= 10; ATTEMPT += 1)); do
+        if ! healthcheck; then
+            echo "Container is not running on attempt #$ATTEMPT, retrying..."
+            sleep 1
+        else
+            echo "Setup complete!"
+            exit 0
+        fi
+    done
+
+    exit 1
+}
+
+healthcheck() {
+    docker ps | grep -q justinpaulhammond-justinpaulhammond-1
 }
 
 main
