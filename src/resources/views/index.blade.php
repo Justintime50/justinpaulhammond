dcd6b02b3c8646bb7b19aafde61d22f1583b537e
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2024-12-03 10:42:45 -0700

    fix: use backblaze for images

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -9,7 +9,7 @@
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     <script src="https://kit.fontawesome.com/0dd4ecd465.js" crossorigin="anonymous"></script>
     <meta name="keywords"
-        content="justin, paul, hammond, web, design, it, business, resume, pro, developer, software, engineer, experience, management">
+          content="justin, paul, hammond, web, design, it, business, resume, pro, developer, software, engineer, experience, management">
     <meta name="description" content="Justin Hammond - Senior Software Engineer, Technical Founder">
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-140798052-1"></script>
@@ -27,22 +27,51 @@
 <body>
     <nav class="navbar navbar-expand-md">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#"><img src="{{ asset('pics/avatar.png') }}" class="justin-head">
-                {{ config('app.name') }}</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTop"
-                aria-controls="navbarTop" aria-expanded="false" aria-label="Toggle navigation">
+            <a class="navbar-brand" href="#">
+                <img class="justin-head"
+                     src="https://f000.backblazeb2.com/file/shield-management/justinpaulhammond/avatar.png"
+                     alt="Justin">
+                {{ config('app.name') }}
+            </a>
+            <button class="navbar-toggler"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarTop"
+                    type="button"
+                    aria-controls="navbarTop"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="navbar-collapse justify-content-end collapse" id="navbarTop">
                 <ul class="navbar-nav ml-auto">
-                    <li class="nav-item"><a href="https://blog.justinpaulhammond.com" target="_blank"
-                            class="nav-link">Blog</a></li>
-                    <li class="nav-item"><a href="https://github.com/justintime50" target="_blank"
-                            class="nav-link">GitHub</a></li>
-                    <li class="nav-item"><a href="https://youtube.com/c/justinhammond" target="_blank"
-                            class="nav-link">YouTube</a></li>
-                    <li class="nav-item"><a href="https://websites.justinpaulhammond.com" target="_blank"
-                            class="nav-link">Websites</a></li>
+                    <li class="nav-item">
+                        <a class="nav-link"
+                           href="https://blog.justinpaulhammond.com"
+                           target="_blank">
+                            Blog
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link"
+                           href="https://github.com/justintime50"
+                           target="_blank">
+                            GitHub
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link"
+                           href="https://youtube.com/c/justinhammond"
+                           target="_blank">
+                            YouTube
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link"
+                           href="https://websites.justinpaulhammond.com"
+                           target="_blank">
+                            Websites
+                        </a>
+                    </li>
                 </ul>
             </div>
         </div>
@@ -51,10 +80,12 @@
     <div class="bg-banner text-center">
         <h1>Hey, I'm Justin Hammond 👋</h1>
         <h3>Senior Software Engineer, Technical Founder</h3>
-        <img src="{{ asset('pics/justin-transparent.png') }}" class="justin-banner-image">
+        <img class="justin-banner-image"
+             src="https://f000.backblazeb2.com/file/shield-management/justinpaulhammond/justin-transparent.png"
+             alt="Justin Hammond">
     </div>
 
-    <div id="about" class="container">
+    <div class="container" id="about">
         <h2>About Me</h2>
         <p class="text-center">I love all things tech. I've been programming for 18+ years, tinkering with electronics
             for 15+ years, and founding or building tech companies for 10+ years. I'm an open source fanatic, Apple
@@ -96,7 +127,7 @@
     </div>
 
     <div class="bg-blue">
-        <div id="engineer" class="container text-center">
+        <div class="container text-center" id="engineer">
             <h2>Software Engineering</h2>
             <p class="pa-slideanim">I have experience with a wide array of languages and frameworks. Listed below is a
                 small sample of items I'm proficient with.</p>
@@ -173,17 +204,27 @@
     <div class="container">
         <h2>News &#38; Articles</h2>
         <ul class="news-table-items">
-            <li class="pa-slideanim"><a
-                    href="https://www.easypost.com/blog/2022-08-04-lessons-learned-overhauling-client-libraries-across-seven-languages"
-                    target="_blank">Lessons Learned Overhauling Client Libraries Across 7 Languages (EasyPost)</a>
+            <li class="pa-slideanim">
+                <a href="https://www.easypost.com/blog/2022-08-04-lessons-learned-overhauling-client-libraries-across-seven-languages"
+                   target="_blank">
+                    Lessons Learned Overhauling Client Libraries Across 7 Languages (EasyPost)
+                </a>
             </li>
-            <li class="pa-slideanim"><a
-                    href="https://utah.comcast.com/2013/05/20/congratulations-comcast-leaders-and-achievers-scholarship-winners/"
-                    target="_blank">Utah $10,000 Leaders &#38; Achievers Award Recipient</a></li>
-            <li class="pa-slideanim"><a href="https://chhighschool.wordpress.com/tag/justin-hammond/"
-                    target="_blank">Copper Hills High School Comcast Scholarship Recognition</a></li>
-            <li class="pa-slideanim">See <a href="https://blog.justinpaulhammond.com" target="_blank">my blog</a>
-                for additional articles.</li>
+            <li class="pa-slideanim">
+                <a href="https://utah.comcast.com/2013/05/20/congratulations-comcast-leaders-and-achievers-scholarship-winners/"
+                   target="_blank">
+                    Utah $10,000 Leaders &#38; Achievers Award Recipient
+                </a>
+            </li>
+            <li class="pa-slideanim">
+                <a href="https://chhighschool.wordpress.com/tag/justin-hammond/" target="_blank">
+                    Copper Hills High School Comcast Scholarship Recognition
+                </a>
+            </li>
+            <li class="pa-slideanim">
+                See <a href="https://blog.justinpaulhammond.com" target="_blank">my blog</a>
+                for additional articles.
+            </li>
         </ul>
     </div>
 
